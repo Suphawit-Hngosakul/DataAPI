2482dd8989bf9a7b1e42ff0931501d42669547bb
Nattasak Chonmanat <nattasak.cho@dome.tu.ac.th>
2025-10-04 14:17:20 +0700

    return projectID and datastreamID in response

--- cloud/Api/project_handler.py
+++ cloud/Api/project_handler.py
@@ -5,8 +5,42 @@ import requests
 def lambda_handler(event, context):
     url = os.environ.get("FROST_SERVER_URL")
     method = event['requestContext']['http']['method']
+
+    if method == "GET":
+        return handle_get(event, url)
+
     if method == "POST":
         return handle_post(event, url)
+
+
+def handle_get(event, url):
+    url = url + "Things"
+    if not url:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": "Environment variable FROST_SERVER_URL is not set"})
+        }
+
+    try:
+        response = requests.get(url, timeout=10)
+        if response.status_code == 200:
+            data = response.json()
+            return {
+                "statusCode": 200,
+                "body": json.dumps(data.get("value", []), indent=2)
+            }
+        else:
+            return {
+                "statusCode": response.status_code,
+                "body": json.dumps({"error": "FROST server returned error"})
+            }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
+
+import json
 
 def handle_post(event, url):
     url = url + "Datastreams"
@@ -104,9 +138,26 @@ def handle_post(event, url):
     try:
         response = requests.post(url, json=Datastream)
         response.raise_for_status()
+        location = response.headers.get("Location")
+        datastream_id = location.split("(")[-1].replace(")", "")
+        print("Created datastream with @iot.id =", datastream_id)
+
+        thing_id = None
+        thing_res = requests.get(url+f"({datastream_id})/Thing", timeout=10)
+        print("Created datastream with @iot.id =", datastream_id)
+        
+        if thing_res.status_code == 200:
+            thing = thing_res.json()
+            print(thing)
+            thing_id = thing["@iot.id"]
+
         return {
             "statusCode": 201,
-            "body": json.dumps({"message": "Datastream created successfully"}, indent=2)
+            "body": json.dumps({
+                "message": "Datastream created successfully",
+                "datastream_id": int(datastream_id),
+                "project_id" : thing_id
+            }, indent=2)
         }
     except Exception as e:
         return {
